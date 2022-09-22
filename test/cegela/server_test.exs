3adbd784c4a85b42ca0ee3675587010f446bdacf
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2022-09-22 12:21:48 -0300

    use the describe inspect test describe pattern

--- test/cegela/server_test.exs
+++ test/cegela/server_test.exs
@@ -7,28 +7,30 @@ defmodule CegelaTest do
 
   doctest Cegela
 
-  test "redirects to bit.ly" do
-    conn =
-      :get
-      |> conn("/something?query=x")
-      |> Server.call([])
+  describe inspect(&Server.call/2) do
+    test "redirects to bit.ly" do
+      conn =
+        :get
+        |> conn("/something?query=x")
+        |> Server.call([])
 
-    assert ["https://bit.ly/something?query=x"] = get_resp_header(conn, "location")
-  end
+      assert ["https://bit.ly/something?query=x"] = get_resp_header(conn, "location")
+    end
 
-  test "redirects permanently" do
-    conn =
-      :get
-      |> conn("/something?query=x")
-      |> Server.call([])
+    test "redirects permanently" do
+      conn =
+        :get
+        |> conn("/something?query=x")
+        |> Server.call([])
 
-    assert 301 = conn.status
-  end
+      assert 301 = conn.status
+    end
 
-  test "there is no favicon" do
-    assert %Conn{status: 404} =
-             :get
-             |> conn("/favicon.ico")
-             |> Server.call(%{})
+    test "there is no favicon" do
+      assert %Conn{status: 404} =
+               :get
+               |> conn("/favicon.ico")
+               |> Server.call(%{})
+    end
   end
 end
--- test/cegela/static_test.exs
+++ test/cegela/static_test.exs
@@ -4,37 +4,39 @@ defmodule Cegela.StaticTest do
 
   alias Cegela.Static
 
-  test "redirects to routes ported from goo.gl" do
-    url = "http://somewherelse.com"
+  describe inspect(&Static.call/2) do
+    test "redirects to routes ported from goo.gl" do
+      url = "http://somewherelse.com"
 
-    conn =
-      :get
-      |> conn("/krakem")
-      |> Static.call(static_routes: %{"/krakem" => url})
+      conn =
+        :get
+        |> conn("/krakem")
+        |> Static.call(static_routes: %{"/krakem" => url})
 
-    assert [^url] = get_resp_header(conn, "location")
-  end
+      assert [^url] = get_resp_header(conn, "location")
+    end
 
-  test "redirects permanently" do
-    url = "http://somewherelse.com"
+    test "redirects permanently" do
+      url = "http://somewherelse.com"
 
-    conn =
-      :get
-      |> conn("/krakem")
-      |> Static.call(static_routes: %{"/krakem" => url})
+      conn =
+        :get
+        |> conn("/krakem")
+        |> Static.call(static_routes: %{"/krakem" => url})
 
-    assert 301 = conn.status
-  end
+      assert 301 = conn.status
+    end
 
-  test "builds a list of routes ported from goo.gl on init" do
-    url = "http://lmgtfy.com/?q=ultima+vers%C3%A3o+do+ubuntu"
+    test "builds a list of routes ported from goo.gl on init" do
+      url = "http://lmgtfy.com/?q=ultima+vers%C3%A3o+do+ubuntu"
 
-    assert [static_routes: %{"/KrAKm" => ^url}] = Static.init([])
-  end
+      assert [static_routes: %{"/KrAKm" => ^url}] = Static.init([])
+    end
 
-  test "does nothing to something that does not match a static" do
-    conn = conn(:get, "/krakem")
+    test "does nothing to something that does not match a static" do
+      conn = conn(:get, "/krakem")
 
-    assert ^conn = Static.call(conn, static_routes: %{})
+      assert ^conn = Static.call(conn, static_routes: %{})
+    end
   end
 end
