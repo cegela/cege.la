aa9a2bc7cc8e5f4cddbd29dc2f5aa55f7a0aede6
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2026-05-04 20:37:13 -0300

    enable sentry logs

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,12 +1,19 @@
 import Config
 
-config :cegela, Cegela.Server, port: System.get_env("PORT", "4000") |> String.to_integer()
+if config_env() != :test do
+  config :cegela, Cegela.Server, port: System.get_env("PORT", "4000") |> String.to_integer()
+end
 
 config :sentry,
   dsn: System.get_env("SENTRY_DSN"),
   environment_name: config_env(),
   enable_source_code_context: true,
   root_source_code_path: File.cwd!(),
+  logs: [
+    level: :info,
+    excluded_domains: [],
+    metadata: []
+  ],
   tags: %{
     env: "production"
   }
