8e0ee236b10baa0f9cf187121c54a2fcc5a9d4ee
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2026-05-04 20:40:22 -0300

    restore unwanted changes

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,8 +1,6 @@
 import Config
 
-if config_env() != :test do
-  config :cegela, Cegela.Server, port: System.get_env("PORT", "4000") |> String.to_integer()
-end
+config :cegela, Cegela.Server, port: System.get_env("PORT", "4000") |> String.to_integer()
 
 config :sentry,
   dsn: System.get_env("SENTRY_DSN"),
